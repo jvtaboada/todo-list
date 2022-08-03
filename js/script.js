a315230055c2204a2a2d2d59fef70f57d4d4d95e
João Vitor <joao_taboada@hotmail.com>
2022-08-03 17:48:08 -0300

    Add feature to add new tasks

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-alert("JS funcionando");
+//adicionar task nova
+document.getElementById("addTaskBtn").addEventListener("click", function(){
+    //salvando a 'descrição' da task nova : "Arrumar a cama"
+    const newTask = document.getElementById("newTask").value;
+
+    //criando a estrutura HTML da nova task
+    const newTaskLineHtml = document.createElement("li");
+    newTaskLineHtml.className = "task";
+    newTaskLineHtml.innerHTML = `<input type="checkbox"/> <span>${newTask}</span> <button>X</button> <button>Edit</button>`
+
+    //metodo para inserir a nova task no DOM
+    const elementoPai = document.querySelector("#taskList");
+    elementoPai.insertBefore(newTaskLineHtml, elementoPai.firstElementChild);
+});
+
+console.log("teste");
+
+//riscar task checked
+
+
+//remover task
+
+
+//editar task
